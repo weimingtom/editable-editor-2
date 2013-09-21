b132f301069f59b44adbe3f16a57697fb205fb37
shell3000 <shell3000@b9a01cc7-03dd-f6a0-0896-7f231dd594b4>
2013-09-21 13:35:25 +0000

    -update

--- demos/galgame/001/run.bat
+++ demos/galgame/001/run.bat
@@ -1,1 +1,3 @@
-..\..\..\main\FDProject\bin\EditableEditor2_r.swf
+copy /y ..\..\..\main\FDProject\bin\EditableEditor2_r.swf EditableEditor2_r.swf
+EditableEditor2_r.swf
+del /s /q EditableEditor2_r.swf
