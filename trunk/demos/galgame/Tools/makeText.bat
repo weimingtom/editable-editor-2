4d38075d7cb4534984904ddffe6a7a17b41d7039
shell3000 <shell3000@b9a01cc7-03dd-f6a0-0896-7f231dd594b4>
2013-10-03 19:00:16 +0000

    -update

--- trunk/demos/galgame/Tools/makeText.bat
+++ trunk/demos/galgame/Tools/makeText.bat
@@ -1,16 +1,18 @@
-ECHO ...dealing String...
 @echo off
+ECHO ...dealing String...
+
 
 if exist *.txt del /s *.txt>NUL
 
-ExportCsv\ExportCsv.exe text.csv
+..\Tools\ExportCsv\ExportCsv.exe text.csv
 
 if exist STR_*_LIB.txt del /s STR_*_LIB.txt >NUL
 
 rem ##############
 
 for %%i in (STR_???.txt) do (
-	StringToBin\StringToBin.exe %%i %%~ni.bin
+	..\Tools\StringToBin\StringToBin.exe %%i %%~ni.bin
 )
 
 if exist STR.h del /s STR.h >NUL
+if exist STR_CHS.bin move /y STR_CHS.bin data.bin
